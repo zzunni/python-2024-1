4bce19dbe097924c3e810e32463d522b5b94094e
zzunni <12201309@inha.edu>
2024-03-18 19:45:46 +0900

    day2-11

--- day2.py
+++ day2.py
@@ -1,8 +1,8 @@
-temp = int(input("온도를 입력하시오: "))
+a = int(input("삼각형의 한 변을 입력하시오: "))
+b = int(input("삼각형의 한 변을 입력하시오: "))
+c = int(input("삼각형의 한 변을 입력하시오: "))
 
-if temp >= 100:
-    print('물의 상태는 기체입니다.')
-elif 100 > temp > 0:
-    print('물의 상태는 액체입니다.')
-elif temp <= 0:
-    print('물의 상태는 고체입니다.')
+if a<b+c and b<a+c and c<a+b:
+    print("올바른 삼각형")
+else:
+    print("유효하지 않은 삼각형")
