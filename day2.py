e0f187c65a5e8d7484188b710107c88db731300a
zzunni <12201309@inha.edu>
2024-03-18 19:32:28 +0900

    day2-10

--- day2.py
+++ day2.py
@@ -1,12 +1,8 @@
-score = int(input("점수를 입력하세요: "))
+temp = int(input("온도를 입력하시오: "))
 
-if score >= 90:
-    print('A')
-elif score >= 80:
-    print('B')
-elif score >= 70:
-    print('C')
-elif score >= 60:
-    print('D')
-else:
-    print('F')
+if temp >= 100:
+    print('물의 상태는 기체입니다.')
+elif 100 > temp > 0:
+    print('물의 상태는 액체입니다.')
+elif temp <= 0:
+    print('물의 상태는 고체입니다.')
