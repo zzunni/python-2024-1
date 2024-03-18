12ddde81e4a65522f3299e071c5cd46c79633b69
zzunni <12201309@inha.edu>
2024-03-18 18:47:49 +0900

    day2-6

--- day2.py
+++ day2.py
@@ -1,8 +1,21 @@
-grade = float(input("학점은? "))
-toeic = int(input("토익점수는? "))
+print("==========================")
+print("메뉴1번: 치즈버거")
+print("메뉴2번: 치킨버거")
+print("메뉴3번: 불고기버거")
+print("==========================\n")
 
-if grade >= 4.0 and toeic >= 800:
-    print("장학금을 받을 수 있습니다.")
+while True:
+    menu_num = int(input("메뉴를 선택하세요: "))
+    if menu_num == 1:
+        print("치즈버거")
+        break
+    elif menu_num ==2:
+        print("치킨버거")
+        break
+    elif menu_num ==3:
+        print("불고기버거")
+        break
+    else:
+        print("잘못 입력하셨습니다.\n")
+        continue
 
-else:
-    print("이번 학기는 장학금을 받을 수 없습니다.")
