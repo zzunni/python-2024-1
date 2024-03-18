579625e69e806999ba75efb02458683c61b0086a
zzunni <12201309@inha.edu>
2024-03-18 20:45:39 +0900

    day2-14

--- day2.py
+++ day2.py
@@ -1,8 +1,7 @@
-a = int(input("시작값 입력: "))
-b = int(input("끝값 입력: "))
-c = int(input("증가값 입력: "))
+num = int(input("정수를 입력하시오: "))
 
-sum = 0
-for i in range(a,b,c):
-    sum = sum + i
-print(f"{a}부터 {b}까지 {c}씩 증가한 값의 합은 {sum}이다.")
+fact = 1
+for i in range(num,0,-1):
+    fact = fact * i
+
+print(f"{num}!은 {fact}이다.")
