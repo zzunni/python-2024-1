aed39a5873ed6ee1e8197a4faa125e6f1988f27d
zzunni <12201309@inha.edu>
2024-03-18 20:35:32 +0900

    day2-13

--- day2.py
+++ day2.py
@@ -1,16 +1,8 @@
-born_date = int(input("출생년도 끝자리 입력: "))
-age = int(input("만 나이를 입력하세요: "))
+a = int(input("시작값 입력: "))
+b = int(input("끝값 입력: "))
+c = int(input("증가값 입력: "))
 
-if age >= 65:
-    print("언제든지 구매 가능합니다.")
-else:
-    if born_date == 1 or born_date == 6:
-        print("월요일에 구매 가능합니다.")
-    elif born_date == 2 or born_date == 7:
-        print("화요일에 구매 가능합니다.")
-    elif born_date == 3 or born_date == 8:
-        print("수요일에 구매 가능합니다.")
-    elif born_date == 4 or born_date == 9:
-        print("목요일에 구매 가능합니다.")
-    else:
-        print("금요일에 구매 가능합니다.")
+sum = 0
+for i in range(a,b,c):
+    sum = sum + i
+print(f"{a}부터 {b}까지 {c}씩 증가한 값의 합은 {sum}이다.")
