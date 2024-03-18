f6c71f69453c72781fc33829560963933e88477c
zzunni <12201309@inha.edu>
2024-03-18 21:21:48 +0900

    day2-17

--- day2.py
+++ day2.py
@@ -1,4 +1,4 @@
-for i in range(1,6):
-    for j in range(1,11):
-        print('*'*j ,end="")
-    print()
+for i in range(1,7):
+    for j in range(6,0,-1):
+        if i+j == 6:
+            print(f"첫번째 주사위={i}, 두번째 주사위={j}")
