1536760c27cc338b9f235e8bbbbe310d88037e1d
zzunni <12201309@inha.edu>
2024-03-18 19:52:21 +0900

    day2-12

--- day2.py
+++ day2.py
@@ -1,8 +1,16 @@
-a = int(input("삼각형의 한 변을 입력하시오: "))
-b = int(input("삼각형의 한 변을 입력하시오: "))
-c = int(input("삼각형의 한 변을 입력하시오: "))
+born_date = int(input("출생년도 끝자리 입력: "))
+age = int(input("만 나이를 입력하세요: "))
 
-if a<b+c and b<a+c and c<a+b:
-    print("올바른 삼각형")
+if age >= 65:
+    print("언제든지 구매 가능합니다.")
 else:
-    print("유효하지 않은 삼각형")
+    if born_date == 1 or born_date == 6:
+        print("월요일에 구매 가능합니다.")
+    elif born_date == 2 or born_date == 7:
+        print("화요일에 구매 가능합니다.")
+    elif born_date == 3 or born_date == 8:
+        print("수요일에 구매 가능합니다.")
+    elif born_date == 4 or born_date == 9:
+        print("목요일에 구매 가능합니다.")
+    else:
+        print("금요일에 구매 가능합니다.")
