6baa4fd925799268bfd9d1afe70b6ed77895f361
zzunni <12201309@inha.edu>
2024-03-18 21:02:38 +0900

    day2-16

--- day2.py
+++ day2.py
@@ -1,5 +1,4 @@
-num = int(input("정수를 입력하시오: "))
-
-for i in range(1,10):
-    ans = num*i
-    print(f"{num} * {i} = {ans}")
+for i in range(1,6):
+    for j in range(1,11):
+        print('*'*j ,end="")
+    print()
