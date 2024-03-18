d74ad8b4ed51840d01bc01049dfcb065d313e4b2
zzunni <12201309@inha.edu>
2024-03-18 18:56:57 +0900

    day2-7

--- day2.py
+++ day2.py
@@ -1,21 +1,14 @@
-print("==========================")
-print("메뉴1번: 치즈버거")
-print("메뉴2번: 치킨버거")
-print("메뉴3번: 불고기버거")
-print("==========================\n")
+today_num = int(input("오늘날짜(숫자입력): "))
+car_num = int(input("차량번호 4자리를 입력하세요: "))
 
-while True:
-    menu_num = int(input("메뉴를 선택하세요: "))
-    if menu_num == 1:
-        print("치즈버거")
-        break
-    elif menu_num ==2:
-        print("치킨버거")
-        break
-    elif menu_num ==3:
-        print("불고기버거")
-        break
+if today_num % 2 == 0:
+    print("오늘은 짝수차량 입장가능.")
+    if car_num % 2 == 0:
+        print("귀하의 차량은 입차 가능합니다.")
     else:
-        print("잘못 입력하셨습니다.\n")
-        continue
-
+        print("귀하의 차량은 입차 불가합니다.")
+else:
+    if car_num % 2 == 0:
+        print("귀하의 차량은 입차 불가합니다.")
+    else:
+        print("귀하의 차량은 입차 가능합니다.")
