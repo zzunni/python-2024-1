61986464b52d485c9c4cee4485fb757c794197b4
zzunni <12201309@inha.edu>
2024-03-25 18:50:15 +0900

    day3-5

--- day3.py
+++ day3.py
@@ -1,6 +1,8 @@
-num = int(input("원하는 단은: "))
-i = 1
+password = "pythonisfun"
+inputpass = input("암호를 입력하세요: ")
 
-while i <= 9:
-    print(f"{num} * {i} = {num*i}")
-    i=i+1
+while password != inputpass:
+    inputpass = input("암호를 입력하세요: ")
+
+print("로그인 성공")
+
