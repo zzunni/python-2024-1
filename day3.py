5b613b9201a85014994800953807ebb3f61f9837
zzunni <12201309@inha.edu>
2024-03-25 20:14:35 +0900

    day3-9

--- day3.py
+++ day3.py
@@ -1,17 +1,10 @@
-import random
-
-answer = random.randint(1,100)
-print("1부터 100까지의 숫자를 맞추시오")
-count = 0
-
+command = ""
+total = 0
 while True:
-    input_ans = int(input("숫자를 입력하시오:"))
-    count = count + 1
-    if input_ans < answer:
-        print("너무 낮음!")
-    elif input_ans > answer:
-        print("너무 높음!")
-    else:
-        print(f"축하합니다. 시도횟수:{count}")
+    num = int(input("숫자를 입력하세요: "))
+    command = input("계속?(yes/no): ")
+    total = total + num
+    if command == "no":
         break
 
+print(f"합계는 {total}")
