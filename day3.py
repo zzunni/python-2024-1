63a8c44e9fd0d946c48ef5cb8ed4231f6d7d1479
zzunni <12201309@inha.edu>
2024-03-25 20:24:54 +0900

    day3-10

--- day3.py
+++ day3.py
@@ -1,10 +1,13 @@
-command = ""
-total = 0
+fruits = ['사과', '배', '딸기', '복숭아']
+
 while True:
-    num = int(input("숫자를 입력하세요: "))
-    command = input("계속?(yes/no): ")
-    total = total + num
-    if command == "no":
+    input_fruit = input("사고싶은 과일은 무엇입니까? ")
+
+    if  input_fruit in fruits:
+        print(f"네. {input_fruit}가 있습니다.")
+    elif input_fruit == '끝':
+        print("안녕히 가십시오.")
         break
 
-print(f"합계는 {total}")
+    else:
+        print(f"지금 우리 가게에는 {input_fruit}이 없습니다.")
