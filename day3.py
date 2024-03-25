6e482fd31c80c46b1a0e637bded05d643570253e
zzunni <12201309@inha.edu>
2024-03-25 21:04:49 +0900

    day3-12

--- day3.py
+++ day3.py
@@ -1,9 +1,10 @@
-for i in range(0,10):
-    if i % 3 == 0 and i % 5 == 0:
-        print("fizzbuzz")
-    elif i % 3 == 0:
-        print("fizz")
-    elif i % 5 == 0:
-        print("buzz")
-    else:
-        print("*")
+num1, num2, num3 = map(int, input("숫자 세개를 입력하세요: ").split())
+max = num1
+
+if max < num2:
+    max = num2
+
+if max < num3:
+    max = num3
+
+print(f"최대값은 {max}입니다.")
