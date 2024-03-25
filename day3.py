4d61100afaf32d18b2890b5c1f59f5e4773b3e02
zzunni <12201309@inha.edu>
2024-03-25 18:51:30 +0900

    day3-5-sol

--- day3.py
+++ day3.py
@@ -1,8 +1,7 @@
-password = "pythonisfun"
-inputpass = input("암호를 입력하세요: ")
+password = ""
 
-while password != inputpass:
-    inputpass = input("암호를 입력하세요: ")
+while password != "pythonisfun":
+    password = input("암호를 입력하세요: ")
 
 print("로그인 성공")
 
