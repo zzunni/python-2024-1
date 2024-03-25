f2f436049550802c459e88104d9fcca68bb402b6
zzunni <12201309@inha.edu>
2024-03-25 20:58:22 +0900

    day3-11

--- day3.py
+++ day3.py
@@ -1,13 +1,9 @@
-fruits = ['사과', '배', '딸기', '복숭아']
-
-while True:
-    input_fruit = input("사고싶은 과일은 무엇입니까? ")
-
-    if  input_fruit in fruits:
-        print(f"네. {input_fruit}가 있습니다.")
-    elif input_fruit == '끝':
-        print("안녕히 가십시오.")
-        break
-
+for i in range(0,10):
+    if i % 3 == 0 and i % 5 == 0:
+        print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
     else:
-        print(f"지금 우리 가게에는 {input_fruit}이 없습니다.")
+        print("*")
