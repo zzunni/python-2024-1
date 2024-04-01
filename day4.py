c450e8a1ae53af513d500566aa8cef1f1285d499
zzunni <12201309@inha.edu>
2024-04-01 20:47:59 +0900

    day4-7

--- day4.py
+++ day4.py
@@ -1,13 +1,12 @@
-def cal_score(*scores):
-    total = 0
-    num = len(scores)
-    for score in scores:
-        total = total + score
+def weeklypay(rate,hour):
+    if hour>=30:
+        pay = 30*rate + (hour-30)*rate*1.5
+    else:
+        pay = hour * rate
 
-    print(f"총점: {total}점")
-    print(f"평점: {total/num}점")
+    return pay
 
-cal_score(90, 30, 80)
+rate = int(input("시급을 입력하세요: "))
+hour = int(input("근무 시간을 입력하세요: "))
 
-
-
+print(f"주급은 {weeklypay(rate, hour)}")
