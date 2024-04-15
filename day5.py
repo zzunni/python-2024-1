0f35cc4700c652fb1f38296c7e6c8836215fe032
zzunni <12201309@inha.edu>
2024-04-15 18:12:10 +0900

    day6-1

--- day5.py
+++ day5.py
@@ -1,19 +1,3 @@
-blood = []
+import random
+score = []
 
-while True:
-    blood.append(input("혈액형을 선택하세요(A,B,AB,O): "))
-    print("헌혈해주셔서 감사합니다. ")
-
-    print("--------------------")
-    print(f"A형: {blood.count('A')}")
-    print(f"B형: {blood.count('B')}")
-    print(f"AB형: {blood.count('AB')}")
-    print(f"O형: {blood.count('O')}")
-
-    command = input("입력을 계속할까요?(Yes/No): ")
-
-    if command == 'Yes':
-        continue
-
-    elif command == 'No':
-        break
---
+++ day6.py
@@ -0,0 +1,6 @@
+# 얕은 복사, 깊은 복사
+temps = [28.31,33,35,27,26,25]
+values = temps
+
+id(temps)
+id(values)
