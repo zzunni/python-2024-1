0f37c097c43840d0db2366936b252516da837987
zzunni <12201309@inha.edu>
2024-04-15 18:56:52 +0900

    day6-5

--- day6.py
+++ day6.py
@@ -1,5 +1,13 @@
+s = [[1,2,3,4,5],
+     [6,7,8,9,10],
+     [11,12,13,14,15]]
 
-numlist = [x for x in range(100) if x%2 == 0 and x%3 ==0]
-print(numlist)
 
+rows = len(s)
+cols = len(s[0])
 
+for r in range(rows):
+    for c in range(cols):
+        print(s[r][c], end=',')
+
+    print()
