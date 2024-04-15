0f1743f0953b83c5ad25d901278ba710dfb526ab
zzunni <12201309@inha.edu>
2024-04-15 18:29:21 +0900

    day6-3

--- day6.py
+++ day6.py
@@ -1,6 +1,7 @@
-# 얕은 복사, 깊은 복사
-a = 10
-if a%2 == 0:
-    print("짝수")
-else:
-    print("홀수")
+x = int(input(" 첫번째 수 = "))
+y = int(input(" 두번째 수 = "))
+
+max_value = (x if x>y else y)
+min_value = (x if x<y else y)
+
+print(f"큰 수 = {max_value}, 작은 수 = {min_value}")
