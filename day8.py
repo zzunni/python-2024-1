c9550792d58f71f611614a87914aea98c4ef1bd8
zzunni <12201309@inha.edu>
2024-04-29 20:01:06 +0900

    day8-5

--- day8.py
+++ day8.py
@@ -1,18 +1,6 @@
 a = input("문자열을 입력하시오: ")
 
-sum1 = 0
-sum2 = 0
-sum3 = 0
+new_A = a.split()
 
-for i in range(len(a)):
-    if a[i].isdigit() == True:
-        sum1 +=1
-
-    elif a[i].isspace() == True:
-        sum2 +=1
-
-    elif a[i].isalpha() == True:
-        sum3 +=1
-
-info = {"digit":sum1, "spaces":sum2, "alpha":sum3}
-print(info)
+for i in new_A:
+    print(i[0].upper(),end= '')
