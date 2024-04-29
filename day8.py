e66d969c6b3233a71a260a2cab22d69dce96fe62
zzunni <12201309@inha.edu>
2024-04-29 18:50:17 +0900

    day8-3

--- day8.py
+++ day8.py
@@ -1,8 +1,9 @@
-info = input("주민번호 13자리 입력: ")
+munja = input("문자열 입력: ")
 
-if int(info[6]) == 1 or int(info[6]) == 3:
-    print("남자입니다.")
+if munja[:] == munja[::-1]:
+    print("회문입니다.")
 
-elif int(info[6]) == 2 or int(info[6]) == 4:
-    print("여자입니다.")
+else:
+    print("회문이 아닙니다.")
+
 
