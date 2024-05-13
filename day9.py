88274af727317856a00baa6803b1652efa0162cd
zzunni <12201309@inha.edu>
2024-05-13 19:07:40 +0900

    day9-3

--- day9.py
+++ day9.py
@@ -1,13 +1,17 @@
-class Television:
-    def __init__(self,channel,volume,on):
-        self.channel = channel
-        self.volume = volume
-        self.on = on
-    def show(self):
-        print(self.channel, self.volume, self.on)
+class Circle:
+    def __init__(self,r):
+        self.r= r
 
-    def setChannel(self, channel):
-        self.channel = channel
+    def getArea(self):
+        area = self.r * self.r * 3.141592
+        return area
 
-    def getChannel(self):
-        return self.channel
+    def getPerimeter(self):
+        perimeter = 2 * self.r * 3.141592
+        return perimeter
+
+
+c = Circle(10)
+
+print(f"원의 면적 {c. getArea()}")
+print(f"원의 둘레 {c.getPerimeter()}")
