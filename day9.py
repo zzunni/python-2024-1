28f9fa8676a941f2865134e53f53d5e683aa3446
zzunni <12201309@inha.edu>
2024-05-13 20:03:10 +0900

    day9-7

--- day9.py
+++ day9.py
@@ -1,21 +1,18 @@
-from random import randint
+class BankAccount:
+    def __init__(self):
+        self.balance = 0
 
-class Dice:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-        self.size = 30
-        self.value = 1
+    def withdaraw(self, amount):
+        self.balance -= amount
+        print("통장에서", amount, "가 출금되었음 ")
+        print("잔액:",self.balance)
 
-    def read_dice(self):
-        return self.value
+    def deposit(self, amount):
+        self.balance += amount
+        print("통장에서", amount, "가 입금되었음 ")
+        print("잔액:",self.balance)
 
-    def print_dice(self):
-        print(f"주사위의 값 {self.value}")
 
-    def roll_dice(self):
-        self.value = randint(1, 6)
-
-d = Dice(100, 100)
-d.roll_dice()
-d.print_dice()
+a = BankAccount()
+a.deposit(100)
+a.withdaraw(10)
