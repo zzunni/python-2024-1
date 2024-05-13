aa72468c473007562c67b898b73eddea01f45b05
zzunni <12201309@inha.edu>
2024-05-13 19:52:44 +0900

    day9-6

--- day9.py
+++ day9.py
@@ -1,13 +1,21 @@
-class Counter:
-    def __init__(self, initValue=0):
-        self.count = initValue
+from random import randint
 
-    def increment(self):
-        self.count += 1
+class Dice:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.size = 30
+        self.value = 1
 
-    def __str__(self):
-        msg = "카운트 값" + str(self.count)
-        return msg
+    def read_dice(self):
+        return self.value
 
-a = Counter(100)
-print(a)
+    def print_dice(self):
+        print(f"주사위의 값 {self.value}")
+
+    def roll_dice(self):
+        self.value = randint(1, 6)
+
+d = Dice(100, 100)
+d.roll_dice()
+d.print_dice()
