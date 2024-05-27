441e7349c78888548129ae1d789c9dae56f07ad3
zzunni <12201309@inha.edu>
2024-05-27 20:00:40 +0900

    day11-2

--- day11.py
+++ day11.py
@@ -1,7 +1,7 @@
+import numpy as np
 import pandas as pd
-data = [1,3,5,7,9]
-a = pd.Series(data, index=['인천','경기','서울','경남','대전'])
 
-print(a)
-print(a.values)
-print(a.index)
+titanic = pd.read_csv("titanic.csv")
+
+print(titanic['Age'].mean())
+
