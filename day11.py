480690d73611152d9c506a41e85340cb1cd26937
zzunni <12201309@inha.edu>
2024-05-27 20:18:50 +0900

    day11-4

--- day11.py
+++ day11.py
@@ -3,4 +3,12 @@ import pandas as pd
 
 titanic = pd.read_csv("titanic.csv")
 
-print(titanic[["Sex","Age"]].groupby("Sex").mean())
+df1 = titanic["Pclass"] == 1
+df2 = titanic["Age"] >= 60
+print(titanic.loc[df1&df2])
+
+df3 = titanic.loc[df1&df2].sort_values(by='Age')
+df4 = titanic.loc[df1|df2]
+
+df3.to_csv('result1.csv',encoding='cp949')
+df4.to_csv('result2.csv',encoding='cp949')
